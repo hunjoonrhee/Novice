76339fce5d84752c03068fb0c7ca61db3c0b8bdf
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-01 17:02:14 +0200

    Namen 5-mal ausgeben mit Hilfe der While-Schleife

--- src/main/java/Novice.java
+++ src/main/java/Novice.java
@@ -1,4 +1,14 @@
 public class Novice {
+
+    public static void main(String[] args) {
+        String firstname = "Hunjoon";
+        String lastname = "Rhee";
+        int n = 0;
+        while(n<5){
+            System.out.println(whatIsYourName(firstname, lastname));
+            n++;
+        }
+    }
     public static String whatIsYourName(String firstname, String lastname){
         String name = firstname + " " + lastname;
         return name;
