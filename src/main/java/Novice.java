b9489bc22f70bf213d2719d54926fb4e1b724620
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-01 17:28:03 +0200

    Add ein Programm, das den Namen des Nutzers einliest und ihn mit Namen begrüßt.

--- src/main/java/Novice.java
+++ src/main/java/Novice.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Novice {
 
     public static void main(String[] args) {
@@ -8,9 +10,25 @@ public class Novice {
             System.out.println(whatIsYourName(firstname, lastname));
             n++;
         }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a name: ");
+        String name = scanner.nextLine();
+
+        System.out.println(sayHello(name));
     }
     public static String whatIsYourName(String firstname, String lastname){
         String name = firstname + " " + lastname;
         return name;
+    }
+
+    public static int square(int number){
+
+        return number*number;
+    }
+
+    public static String sayHello(String name){
+        String greeting = "Hallo, " + name + ", wie geht es dir?";
+        return greeting;
     }
 }
--- src/test/java/NoviceTest.java
+++ src/test/java/NoviceTest.java
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class NoviceTest {
@@ -16,5 +19,45 @@ public class NoviceTest {
       // THEN
       assertEquals("Hunjoon Rhee", actual);
    }
+
+   @Test
+   void squareTestForNegativeNumber(){
+      // GIVEN
+      int n = -15;
+
+      // WHEN
+      int actual = Novice.square(n);
+
+      // THEN
+      assertEquals(225, actual);
+   }
+
+   @Test
+   void squareTestForZero(){
+      // GIVEN
+      int n = 0;
+
+      // WHEN
+      int actual = Novice.square(n);
+
+      // THEN
+      assertEquals(0, actual);
+   }
+
+   @Test
+   void sayHelloTest(){
+      // GIVEN
+
+      String Name = "Joon";
+
+
+
+      // WHEN
+      String actual = Novice.sayHello(Name);
+
+      // THEN
+      assertEquals("Hallo, " + Name + ", wie geht es dir?", actual);
+   }
+
 
 }
